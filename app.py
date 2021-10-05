e668c025ee55cda52e63fb5db5572dcabdbd7d92
rtkilian <rhys.kilian12@gmail.com>
2021-10-05 20:00:50 +1100

    Render a template and get spacy to load

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+from flask import Flask, render_template
+
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
 
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('spacytextblob')
-text = 'I had a really great day. It was the best day ever! But every now and then I have a really good day that makes me happy.'
-doc = nlp(text)
 
-print(doc._.polarity)
+app = Flask(__name__)
+
+
+@app.route('/')
+def process_sentiment():
+    text = 'This campsite was okay'
+    doc = nlp(text)
+    score = doc._.polarity
+    return render_template('index.html', score=score)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
