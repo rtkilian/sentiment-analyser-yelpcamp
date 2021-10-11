5b87b80912ac0f730983ee5b4a30bbad574cad25
rtkilian <rhys.kilian12@gmail.com>
2021-10-11 18:17:36 +1100

    Modified to use flask_restful

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
@@ -7,16 +8,23 @@ nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('spacytextblob')
 
 app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()  # used to parse incoming requests
+parser.add_argument('review', required=True,
+                    help='Review cannot be blank!')
 
 
-@app.route('/predict', methods=['POST'])
-def process_sentiment():
-    if request.method == 'POST':
-        review = request.form['review']
+class PredictSentiment(Resource):
+    def post(self):
+        args = parser.parse_args()
+        review = args['review']
         doc = nlp(review)
         score = doc._.polarity
         return {'score': score}
 
+
+api.add_resource(PredictSentiment, '/predict')
 
 if __name__ == '__main__':
     app.run(debug=True)
